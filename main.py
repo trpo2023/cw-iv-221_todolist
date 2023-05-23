192adfad08d2ac5628ef4af0ca536e56ac4b7eca
AnarhoKukoldist <yan_antsiferov@DESKTOP-G99FEVK>
2023-05-23 21:07:32 +0700

    format fix

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 import sys
 from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtWidgets import (QPushButton, QLineEdit)
+from PyQt5.QtWidgets import QPushButton, QLineEdit
 from datetime import datetime
 import telebot
 
-TOKEN = '1783146665:AAEKE0MoajwKlWVOp8gKTzZiJ-KUOKJXpC8'
+TOKEN = "1783146665:AAEKE0MoajwKlWVOp8gKTzZiJ-KUOKJXpC8"
 bot = telebot.TeleBot(TOKEN)
 
 telegram_id = 0
@@ -16,11 +16,12 @@ file.close()
 
 try:
     UsrInfo = str((bot.get_chat_member(telegram_id, telegram_id).user))
-    UsrInfo = UsrInfo[UsrInfo.find('username') + 12 :]
+    UsrInfo = UsrInfo[UsrInfo.find("username") + 12 :]
     UsrInfo = UsrInfo.partition("'")[0]
-    UsrInfo = 'Привет, ' + UsrInfo + '!'
+    UsrInfo = "Привет, " + UsrInfo + "!"
 except:
-    UsrInfo = 'Введите Telegram ID'
+    UsrInfo = "Введите Telegram ID"
+
 
 class Widget(QtWidgets.QWidget):
     clicked = QtCore.pyqtSignal()
@@ -29,7 +30,7 @@ class Widget(QtWidgets.QWidget):
 
     def __init__(self, parent=None):
         super(Widget, self).__init__(parent)
-        self.le = QLineEdit()   
+        self.le = QLineEdit()
         delete_button = QPushButton("✓")
         delete_butto = QPushButton("X")
         hlay = QtWidgets.QHBoxLayout(self)
@@ -37,7 +38,8 @@ class Widget(QtWidgets.QWidget):
         hlay.addWidget(delete_button)
         hlay.addWidget(delete_butto)
         delete_button.clicked.connect(self.clicked)
-        delete_butto.clicked.connect(self.clicke) 
+        delete_butto.clicked.connect(self.clicke)
+
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
@@ -46,7 +48,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setWindowTitle("Планировщик задач")
 
         self.telegram_id_le = QtWidgets.QLineEdit()
-        self.telegram_id_le.setPlaceholderText('Введите ваш Telegram ID (его можно узнать с помощью бота "TelegramId")')
+        self.telegram_id_le.setPlaceholderText(
+            'Введите ваш Telegram ID (его можно узнать с помощью бота "TelegramId")'
+        )
         self.telegram_id_le.returnPressed.connect(self.save_telegram_id)
         self.statusBar().addWidget(self.telegram_id_le, 1)
 
@@ -70,17 +74,20 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.setCentralWidget(self.tab_widget)
 
-        
     def save_telegram_id(self):
         telegram_id = self.telegram_id_le.text()
         try:
             int(telegram_id)
         except ValueError:
-            QtWidgets.QMessageBox.critical(self, "Ошибка", "Telegram ID должен быть числом!")
+            QtWidgets.QMessageBox.critical(
+                self, "Ошибка", "Telegram ID должен быть числом!"
+            )
             return
         with open("ID.txt", "w") as f:
             f.write(telegram_id)
-        self.statusBar().showMessage("Telegram ID сохранен, перезапустите приложение для связи с Telegram")
+        self.statusBar().showMessage(
+            "Telegram ID сохранен, перезапустите приложение для связи с Telegram"
+        )
 
     def add(self):
         it = QtWidgets.QListWidgetItem()
@@ -90,8 +97,13 @@ class MainWindow(QtWidgets.QMainWindow):
         widget.clicke.connect(self.delete1)
         self.others_commands_widget.setItemWidget(it, widget)
         it.setSizeHint(widget.sizeHint())
-        f = open ('история.txt','a')
-        f.write('\n' + datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M") + ' Добавлен пункт' + '\n')
+        f = open("история.txt", "a")
+        f.write(
+            "\n"
+            + datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M")
+            + " Добавлен пункт"
+            + "\n"
+        )
         f.close()
 
     def delete(self):
@@ -99,9 +111,14 @@ class MainWindow(QtWidgets.QMainWindow):
         gp = widget.mapToGlobal(QtCore.QPoint())
         lp = self.others_commands_widget.viewport().mapFromGlobal(gp)
         row = self.others_commands_widget.row(self.others_commands_widget.itemAt(lp))
-        t_it = self.others_commands_widget.takeItem(row)        
-        f = open ('история.txt','a')
-        f.write('\n' + datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M") + ' Пункт выполнен' + '\n')
+        t_it = self.others_commands_widget.takeItem(row)
+        f = open("история.txt", "a")
+        f.write(
+            "\n"
+            + datetime.strftime(datetime.now(), "%d.%m.%Y %H:%M")
+            + " Пункт выполнен"
+            + "\n"
+        )
         f.close()
         del t_it
 
@@ -115,17 +132,36 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def ReadText_fn(self):
         try:
-            bot.send_message(telegram_id,"📝" )
-            bot.send_message(telegram_id,"ДОБАВЛЕНО " + datetime.strftime(datetime.now(), "%d.%m.%Y") )
-        except: pass
+            bot.send_message(telegram_id, "📝")
+            bot.send_message(
+                telegram_id,
+                "ДОБАВЛЕНО " + datetime.strftime(datetime.now(), "%d.%m.%Y"),
+            )
+        except:
+            pass
 
-        for index in range(0,self.others_commands_widget.count()):
-            TargetItem = self.others_commands_widget.itemWidget(self.others_commands_widget.item(index)).children()[1].text()
+        for index in range(0, self.others_commands_widget.count()):
+            TargetItem = (
+                self.others_commands_widget.itemWidget(
+                    self.others_commands_widget.item(index)
+                )
+                .children()[1]
+                .text()
+            )
             print(TargetItem)
-            f = open ('невыполенное.txt','a')
-            f.write('\n' + ' Не выполнено за' + " " + datetime.strftime(datetime.now(), "%d.%m.%Y") +':' + TargetItem + '\n')
+            f = open("невыполенное.txt", "a")
+            f.write(
+                "\n"
+                + " Не выполнено за"
+                + " "
+                + datetime.strftime(datetime.now(), "%d.%m.%Y")
+                + ":"
+                + TargetItem
+                + "\n"
+            )
             f.close()
-                bot.send_message(telegram_id, marginal, parse_mode='MarkdownV2')
+            bot.send_message(telegram_id, marginal, parse_mode="MarkdownV2")
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
